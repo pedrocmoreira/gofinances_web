09a16917b4b5fa99139832aed81ae3777f840df5
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-05-19 21:48:36 -0300

    Add Header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     --red: #E52E4D;
     --blue: #5429CC;
 
-    --blue-ligh: #6933FF;
+    --blue-light: #6933FF;
 
     --text-title: #363F5F;
     --text-body: #969CB3;
