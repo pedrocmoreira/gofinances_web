72a726b7fd4eedff46d663e6fef6caf7615e1ef4
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-05-25 20:38:05 -0300

    Saving data from form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -13,6 +13,14 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
 
   const [type, setType] = useState('deposit');
+  const [ title, setTitle ] = useState('');
+  const [ value, setValue ] = useState(0);
+  const [ category, setCategory ] = useState('');
+
+
+  function handleCreateNewTransaction(){
+    
+  }
 
 
   return (
@@ -26,11 +34,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <img src={CloseImg} alt="Fechar Modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder='Título' />
-        <input type="number" placeholder='Valor' />
+        <input placeholder='Título' value={title} onChange={event => setTitle(event.target.value)}/>
+        <input type="number" placeholder='Valor'  value={value} onChange={event => setValue(Number(event.target.value))} />
         <TransctionTypeContainer>
 
           <RadioBox
@@ -39,7 +47,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             isActive={type === 'deposit'}
             activeColor='green'
           >
-            <img src={IncomeImg} alt="Entrada" />
+            <img src={IncomeImg} alt="Entrada"   />
             <span>Entrada</span>
           </RadioBox>
 
@@ -54,7 +62,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           </RadioBox>
 
         </TransctionTypeContainer>
-        <input placeholder='Categoria' />
+        <input placeholder='Categoria'  value={category} onChange={event => setCategory(event.target.value)}/>
 
         <button type="submit">Cadastrar</button>
       </Container>
